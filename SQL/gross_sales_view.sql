eec9b5f39b87928e4f519d89b14781a4df6ae615
Aakankshasurvi <143276959+Aakankshasurvi@users.noreply.github.com>
2025-07-26 19:36:31 +0530

    Add files via upload

---
+++ SQL/gross_sales_view.sql
@@ -0,0 +1,23 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `gross sales` AS
+    SELECT 
+        `s`.`date` AS `date`,
+        `g`.`fiscal_year` AS `fiscal_year`,
+        `c`.`customer_code` AS `customer_code`,
+        `c`.`customer` AS `customer`,
+        `c`.`market` AS `market`,
+        `p`.`product_code` AS `product_code`,
+        `p`.`product` AS `product`,
+        `p`.`variant` AS `variant`,
+        `s`.`sold_quantity` AS `sold_quantity`,
+        `g`.`gross_price` AS `gross_price`,
+        (`s`.`sold_quantity` * `g`.`gross_price`) AS `gross_price_total`
+    FROM
+        (((`fact_sales_monthly` `s`
+        JOIN `dim_customer` `c` ON ((`s`.`customer_code` = `c`.`customer_code`)))
+        JOIN `dim_product` `p` ON ((`s`.`product_code` = `p`.`product_code`)))
+        JOIN `fact_gross_price` `g` ON (((`s`.`product_code` = `g`.`product_code`)
+            AND (YEAR((`s`.`date` + INTERVAL 4 MONTH)) = `g`.`fiscal_year`))))
